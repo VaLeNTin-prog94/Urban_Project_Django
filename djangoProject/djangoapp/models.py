98b82105cbed52b6f18f4c9aa432babf3c838ad2
VaLeNTin-prog94 <valentinforevo@gmail.com>
2024-10-30 10:21:58 +0300

    Sqlite 3

--- djangoProject/djangoapp/models.py
+++ djangoProject/djangoapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    GENRE_CHOICES = [
+        ('FIC', 'Fictions'),
+        ('NON', 'Non_Fiction'),
+        ('SCI', 'Science Fiction'),
+        ('FAN', 'Fantasy'),
+        ('MYS', 'Mystery'),
+        ('THR', 'Thriller'),
+        ('ROM', 'Romance'),
+        ('HIS', 'Historical'),
+    ]
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    publication_date = models.DateField()
+    genre = models.CharField(
+        max_length=3,
+        choices=GENRE_CHOICES,
+        default='FIC',
+    )
+
+    def __str__(self):
+        return self.title
