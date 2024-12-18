8e047f8f8801f86fdbc5a3a4511e0419642237a6
VaLeNTin-prog94 <valentinforevo@gmail.com>
2024-12-18 11:17:55 +0300

    Задача №2. Реализовать функционал: Удаление объявлений

--- urban_project/board/views.py
+++ urban_project/board/views.py
@@ -3,16 +3,30 @@ from board.models import Advertisement
 from board.forms import AdvertisementForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.contrib import messages
+
 
 def logout_view(request):
+    '''
+    Представление для выхода из систему пользователя
+    :param request: запроос
+    :return: Возвращает домашнюю страницу
+    '''
     logout(request)
     return redirect('home')
+
 
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
 from django.contrib.auth import login, authenticate
 
+
 def signup(request):
+    '''
+    Представление для регистрации в системе пользователя
+    :param request: запрос
+    :return: регистрирует пользователя в системе
+    '''
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -23,31 +37,63 @@ def signup(request):
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
 
+
 def home(request):
+    '''
+    Домашняя страница
+    :param request: запрос
+    :return: возвращает домашнюю страницу
+    '''
     return render(request, 'home.html')
 
+
 def advertisement_list(request):
+    '''
+    Представление для вывода всех объявлений
+    :param request: запрос
+    :return: Возвращает страницу с всеми объявлениями
+    '''
     advertisements = Advertisement.objects.all()
     return render(request, 'board/advertisement_list.html', {'advertisements': advertisements})
 
+
 def advertisement_detail(request, pk):
+    '''
+    Выводит конкретное объявление через id
+    :param request: запрос
+    :param pk: if
+    :return: страницу с id объявлением
+    '''
     advertisement = Advertisement.objects.get(pk=pk)
     return render(request, 'board/advertisement_detail.html', {'advertisement': advertisement})
 
+
 @login_required
-def edit_advertisement(request,pk):
+def edit_advertisement(request, pk):
+    '''
+    Представление для редактирования объявлений
+    :param request: запрос
+    :param pk: id объявления
+    :return: Редактирует объявления и возращает страницу объявлений
+    '''
     advertisement = Advertisement.objects.get(pk=pk)
     if request.method == "POST":
-        form = AdvertisementForm(request.POST, instance = advertisement)
+        form = AdvertisementForm(request.POST, instance=advertisement)
         if form.is_valid():
             advertisement.save()
             return redirect('board:advertisement_list')
     else:
-        form = AdvertisementForm(instance = advertisement)
-    return render(request,'board/edit_advertisement.html',{'form': form, 'advertisement': advertisement})
+        form = AdvertisementForm(instance=advertisement)
+    return render(request, 'board/edit_advertisement.html', {'form': form, 'advertisement': advertisement})
+
 
 @login_required
 def add_advertisement(request):
+    '''
+    Представление для добавления объявления
+    :param request: запрос
+    :return: Добавляет объявление и выводит список представлений
+    '''
     if request.method == "POST":
         form = AdvertisementForm(request.POST)
         if form.is_valid():
@@ -59,3 +105,18 @@ def add_advertisement(request):
         form = AdvertisementForm()
     return render(request, 'board/add_advertisement.html', {'form': form})
 
+
+@login_required
+def delete_advertisement(request, pk):
+    '''
+    Представление для удаления объявления
+    :param request: запрос
+    :param pk: id  объявления
+    :return: Удаляет объявление и возвращает на страницу объявлений
+    '''
+    advertisement = Advertisement.objects.get(pk=pk)
+    if request.method == "POST":
+        advertisement.delete()
+        messages.success(request, 'Объявление успешно удалено.')
+        return redirect('board:advertisement_list')
+    return render(request, 'board/delete_advertisement.html', {'advertisement': advertisement})
